493402ff7b4d113b7ad716426012be35b268c628
islam sayed <islam.sayed8@gmail.com>
2018-06-20 22:54:04 +0200

    fix: change port

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 5555 // Change this to your server port
-    return `data/restaurants.json`;
+    const port = 7777 // Change this to your server port
+    return `http://localhost:${port}/data/restaurants.json`;
   }
 
   /**
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}`);
   }
 
   /**
