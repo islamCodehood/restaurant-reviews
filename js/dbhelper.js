c1b6975c20fd3d583a4c738a8a46579782562f76
Islam Sayed <islam.sayed8@gmail.com>
2018-06-20 10:33:04 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 5555 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `data/restaurants.json`;
   }
 
   /**
