01eb6fc962b029b8ef88b71b17917853057c7b60
Islam Sayed <islam.sayed8@gmail.com>
2018-06-20 09:19:16 +0200

    initialize

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiaXNsYW04IiwiYSI6ImNqaWg1bmx0ejBiMTYzdnBtd200cW1zMXAifQ.M8bzzsiydOCAILLY6gMhIg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -149,6 +149,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  
   name.innerHTML = review.name;
   li.appendChild(name);
 
