5aebc53726a40baeb9266fdc1588981708f93b90
islam sayed <islam.sayed8@gmail.com>
2018-06-24 23:35:57 +0200

    fix: Add alt to images

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = restaurant.name + ' restaurant';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
