4ac3f0ff2f034114383314e44c16f02346e7b390
Islam <islam.sayed8@gmail.com>
2018-06-26 15:21:30 +0200

    fix: Add hidden div

--- js/main.js
+++ js/main.js
@@ -176,10 +176,17 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
+  const hiddenDiv = document.createElement('div');
+  hiddenDiv.innerHTML = restaurant.name + ' restaurant - view details';
+  hiddenDiv.id = restaurant.id;
+  hiddenDiv.hidden = true;
+  li.append(hiddenDiv);
+
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute('aria-labelledby', hiddenDiv.id);
+  li.append(more);
 
   return li
 }
@@ -199,6 +206,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 } 
+
+//to escape map links in older browsers
+document.querySelector('.skip-link').addEventListener('click', function() {
+  document.querySelector('#restaurants').focus();
+});
+
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
