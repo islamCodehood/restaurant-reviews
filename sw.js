60281ceddc09c77e844c7108ac884879b05be466
Islam <islam.sayed8@gmail.com>
2018-06-28 00:04:25 +0200

    fix: Change cache urls

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const currentCache = 'restaurantsCache-v2';
+const currentCache = 'restaurantsCache-v1';
 self.addEventListener('install', function(event) {
     //urls to be cached
     const urlsToCache = [
-        'https://islam888.github.io/restaurant-reviews/',
-        'https://islam888.github.io/restaurant-reviews/css/styles.css',
-        'https://islam888.github.io/restaurant-reviews/data/restaurants.json',
-        'https://islam888.github.io/restaurant-reviews/img/',
-        'https://islam888.github.io/restaurant-reviews/js/main.js',
-        'https://islam888.github.io/restaurant-reviews/js/restaurant_info.js',
-        'https://islam888.github.io/restaurant-reviews/js/dbhelper.js',
+        '/',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/dbhelper.js',
         'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
@@ -35,19 +35,4 @@ self.addEventListener('activate', function(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) return response;
-            return fetch(event.request).then(function(response) {
-                if (response.status === 404) {
-                    //free download and personal use png from https://pngtree.com/freepng/404-error-vector_2871439.html
-                    return fetch('https://islam888.github.io/restaurant-reviews/img/404.png');
-                }
-                return response;
-            })
-        })
-    );
-
-});
 
