87de71e82570e3a2ca9094f07e7dfb9ed0b018a4
Islam <islam.sayed8@gmail.com>
2018-06-28 01:21:34 +0200

    fix: Change cache urls

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-console.log('hi');
+
 var currentCache = 'restaurantsCache-v1';
 self.addEventListener('install', function(event) {
     //urls to be cached
     const urlsToCache = [
-        '/',
-        '/css/styles.css',
-        '/data/restaurants.json',
-        '/img/',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        '/js/dbhelper.js',
+        './',
+        './css/styles.css',
+        './data/restaurants.json',
+        './img/',
+        './js/main.js',
+        './js/restaurant_info.js',
+        './js/dbhelper.js',
         'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
