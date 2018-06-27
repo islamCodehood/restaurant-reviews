a5ce3ea85a4ac30146d46add6a6b24ce8177e212
Islam <islam.sayed8@gmail.com>
2018-06-28 01:58:47 +0200

    fix: Add cache urls

--- sw.js
+++ sw.js
@@ -3,12 +3,24 @@ var currentCache = 'restaurantsCache-v1';
 self.addEventListener('install', function(event) {
     //urls to be cached
     const urlsToCache = [
-        './',
-        './css/styles.css',
-        './data/restaurants.json',
-        './js/main.js',
-        './js/restaurant_info.js',
-        './js/dbhelper.js'
+        '/',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/dbhelper.js',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/img/404.png',
+        '/img/favicon.png'
     ];
     event.waitUntil(
         caches.open(currentCache).then(function(cache) {
