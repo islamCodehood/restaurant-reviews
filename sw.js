81b514b726ddf630d2ec2c58c65114be3b9c5be4
Islam <islam.sayed8@gmail.com>
2018-06-27 22:55:04 +0200

    fix: Delete old caches

--- sw.js
+++ sw.js
@@ -21,3 +21,17 @@ self.addEventListener('install', function(event) {
     
 });
 
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName !== currentCache;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
