c774864c4569c5bf296c5bb23a9f67513e9fea8c
Islam <islam.sayed8@gmail.com>
2018-06-27 23:44:16 +0200

    rl

--- sw.js
+++ sw.js
@@ -42,7 +42,7 @@ self.addEventListener('fetch', function(event) {
             return fetch(event.request).then(function(response) {
                 if (response.status === 404) {
                     //free download and personal use png from https://pngtree.com/freepng/404-error-vector_2871439.html
-                    return fetch('/img/404.png');
+                    return fetch('https://islam888.github.io/restaurant-reviews/img/404.png');
                 }
                 return response;
             })
