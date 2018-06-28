17c1048d6707edbdfc20fad57f0eecdbc96cf200
Islam <islam.sayed8@gmail.com>
2018-06-28 03:10:49 +0200

    fix: modify cache url

--- sw.js
+++ sw.js
@@ -2,25 +2,25 @@ var currentCache = 'restaurantsCache-v2';
 self.addEventListener('install', function(event) {
     //urls to be cached
     const urlsToCache = [
-        './index.html',
-        './restaurant.html',
-        './css/styles.css',
-        './data/restaurants.json',
-        './js/main.js',
-        './js/restaurant_info.js',
-        './js/dbhelper.js',
-        './img/1.jpg',
-        './img/2.jpg',
-        './img/3.jpg',
-        './img/4.jpg',
-        './img/5.jpg',
-        './img/6.jpg',
-        './img/7.jpg',
-        './img/8.jpg',
-        './img/9.jpg',
-        './img/10.jpg',
-        './img/404.png',
-        './img/favicon.png',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/dbhelper.js',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/img/404.png',
+        '/img/favicon.png',
         'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
@@ -56,7 +56,7 @@ self.addEventListener('fetch', function(event) {
             return fetch(event.request).then(function(response) {
                 if (response.status === 404) {
                     //free download and personal use png from https://pngtree.com/freepng/404-error-vector_2871439.html
-                    return fetch('./img/404.png');
+                    return fetch('/img/404.png');
                 }
                 return response;
             })
