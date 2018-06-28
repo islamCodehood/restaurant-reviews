842b3eb80573df43a181e4fbe51afd780921e455
Islam <islam.sayed8@gmail.com>
2018-06-28 14:44:31 +0200

    fix: Remove google map code

--- js/main.js
+++ js/main.js
@@ -232,14 +232,3 @@ if (navigator.serviceWorker) {
 }
 
 
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
-
